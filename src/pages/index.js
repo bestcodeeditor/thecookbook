078532131ea6161aa2a29c0dc0f9707ec4725deb
sudip <sudipzoom@rediffmail.com>
2019-02-24 19:14:52 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Home Page</h1>
+   
+  </Layout>
+)
+
+export default IndexPage
