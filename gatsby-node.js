87bd76f96bc74c9218a893078c9b72595dc7335e
sudip <sudipzoom@rediffmail.com>
2019-03-03 22:04:17 +0530

    tag added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 const path = require("path")
+
 const { slugify } = require("./src/util/utility")
 const authors = require("./src/util/authors")
+const _ = require("lodash")
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
@@ -17,6 +19,12 @@ exports.onCreateNode = ({ node, actions }) => {
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
 
+  // Page templates
+  const templates = {
+    eachPost: path.resolve("./src/templates/singlePost.js"),
+    tagPage: path.resolve("./src/templates/tagsPage.js"),
+  }
+
   return graphql(`
     {
       allMarkdownRemark {
@@ -24,6 +32,7 @@ exports.createPages = ({ actions, graphql }) => {
           node {
             frontmatter {
               author
+              tags
             }
             fields {
               slug
@@ -38,13 +47,37 @@ exports.createPages = ({ actions, graphql }) => {
     posts.forEach(post => {
       createPage({
         path: post.fields.slug,
-        component: path.resolve("./src/templates/singlePost.js"),
+        component: templates.eachPost,
         context: {
           slug: post.fields.slug,
           imageUrl: authors.find(x => x.name === post.frontmatter.author)
             .imageUrl,
         },
       })
+    })
+    // Get all tags
+    let tags = []
+    _.each(posts, edge => {
+      tags = tags.concat(edge.frontmatter.tags)
+    })
+
+    let tagPostCounts = {} // { tutorial: 2, design: 1}
+    tags.forEach(tag => {
+      // Or 0 cause it might not exist yet
+      tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+    })
+
+    // Remove duplicates
+    tags = _.uniq(tags)
+
+    // Tags page (all tags)
+    createPage({
+      path: "/tags",
+      component: templates.tagPage,
+      context: {
+        tags,
+        tagPostCounts,
+      },
     })
   })
 }
